217b20a2bdfcfe2d9d08918ccbfdd6add30e1190
mujthabamkdev <mujthabamkdev@gmail.com>
2025-10-18 18:04:38 +0800

    ui changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./features/home/home.component').then((m) => m.HomeComponent),
+      import('@features/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'auth',
