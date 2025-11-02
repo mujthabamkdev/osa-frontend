5884b1e49287979f71403e002ad888544ac87a59
mujthabamkdev <mujthabamkdev@gmail.com>
2025-11-02 18:50:15 +0800

    Exec nginx directly and default port 8080

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,12 @@
 set -e
 
 if [ -z "$PORT" ]; then
-  PORT=80
+  PORT=8080
 fi
 
 export PORT
 
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-echo "Starting nginx on port $PORT"
-echo "----- Rendered nginx config -----"
-cat /etc/nginx/conf.d/default.conf || true
-echo "-----------------------------------"
+echo "Starting nginx on port $PORT with pid 1"
 exec nginx -g 'daemon off;'
