0a646eef300ca57e310150ae8e76d769b1d97869
mujthabamkdev <mujthabamkdev@gmail.com>
2025-11-02 14:07:44 +0800

    Export PORT before starting nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,9 @@ if [ -z "$PORT" ]; then
   PORT=80
 fi
 
+export PORT
+
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "Starting nginx on port $PORT"
 exec nginx -g 'daemon off;'
