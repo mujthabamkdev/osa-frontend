9522275fa5a3857b08126369d5eb3c1c34452f17
mujthabamkdev <mujthabamkdev@gmail.com>
2025-11-02 15:13:40 +0800

    Log rendered nginx config for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,7 @@ export PORT
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx on port $PORT"
+echo "----- Rendered nginx config -----"
+cat /etc/nginx/conf.d/default.conf || true
+echo "-----------------------------------"
 exec nginx -g 'daemon off;'
