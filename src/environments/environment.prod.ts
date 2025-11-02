88664e5daed8ccf65ef9d1566c3f1896fc297f2f
mujthabamkdev <mujthabamkdev@gmail.com>
2025-11-03 07:12:25 +0800

    Add missing appName and version to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://osa-backend-production.up.railway.app/api/v1'
+  apiUrl: 'https://osa-backend-production.up.railway.app/api/v1',
+  appName: 'Online Sharia Academy',
+  version: '1.0.0',
 };
